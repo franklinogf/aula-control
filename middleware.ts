36eca32bce5029ecd55f6ce1953c13fff18e8e4c
DarwinML2 <darwinmass.ml2@gmail.com>
2024-08-26 17:06:07 -0400

    admin pages

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,1 @@
-export { auth as middleware } from "./src/auth"
-export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  }
+export { auth as middleware } from "@/auth"
